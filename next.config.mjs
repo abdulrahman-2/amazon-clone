94597992a4f921cd0d952938ff9893dadb6cff70
abdulrahman-2 <abdoattallah3@gmail.com>
2024-10-15 14:59:37 +0300

    edit images protcool

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.dummyjson.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    domains: ["cdn.dummyjson.com"],
   },
 };
 
